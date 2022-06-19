2572918a3ffc659ba03b2cfc499bdb805dfd6f47
rinafoxy <andreeva.rina17@gmail.com>
2022-06-19 17:18:50 +0300

    done task 2, 3 and 4

---
+++ app/src/main/java/com/example/introandroid/SecondScreen.java
@@ -0,0 +1,14 @@
+package com.example.introandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+    }
+}
